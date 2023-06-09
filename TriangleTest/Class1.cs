b3f6f7554c8b57f276087031bbff6268449d57d1
PrashantV2 <prashantvirdi719@gmail.com>
2023-06-08 21:24:39 -0400

    Equilateral Test1

--- TriangleTest/Class1.cs
+++ TriangleTest/Class1.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit.Framework;
+using System.Security.Policy;
 
 namespace TriangleTest
 {
-    public class Class1
+    [TestFixture]
+    public class TriangleTests
     {
+        [Test]
+        public void Check_equilateral_Triangle_is_valid()
+        {
+            // Arrange
+            int hypotenuse_side = 8;
+            int opposite_side = 8;
+            int adjacent_side = 8;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(hypotenuse_side,opposite_side, adjacent_side);
+
+            // Assert
+            Assert.AreEqual("The triangle is valid and is an EQUILATERAL", result);
+        }
     }
 }
